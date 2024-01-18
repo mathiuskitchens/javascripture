35260f77b7378a5f66b9368f9d0c9a0bcda4011a
Matthew Kitchens <mathiuskitchens@gmail.com>
2024-01-18 13:27:19 -0500

    fixing again

--- src/Search.jsx
+++ src/Search.jsx
@@ -18,29 +18,10 @@ import {
 import { searchVerse } from "services/api-requests";
 import { useState } from "react";
 import { gptRequest } from "services/openai-requests";
-import { gptRequest } from "services/openai-requests";
 
 function Search() {
   const [searchInput, setSearchInput] = useState("");
   const [resultVerse, setResultVerse] = useState("");
-  const [gptVerse, setGptVerse] = useState("");
-  const [translation, setTranslation] = useState("");
-
-  const handleSearch = async () => {
-    const result = await searchVerse(searchInput);
-    setResultVerse(result);
-    console.log(result);
-  };
-
-  const handleTranslationChange = (event) => {
-    setTranslation(event.target.value);
-  };
-
-  const handleGpt = async () => {
-    const gptResult = await gptRequest(searchInput, translation);
-    setGptVerse(gptResult.message.content);
-    console.log(gptResult);
-  };
   const [gptVerse, setGptVerse] = useState("");
   const [translation, setTranslation] = useState("");
 
@@ -84,49 +65,8 @@ function Search() {
               handleSearch();
             }
           }}
-          onKeyDown={(event) => {
-            if (event.key === "Enter") {
-              handleSearch();
-            }
-          }}
         />
-        <FormControl
-          sx={{ minWidth: 100 }}
-          size="small"
-          style={{ marginLeft: "6px", marginTop: "18px", height: "2rem" }}
-        >
-          <InputLabel id="translation-selector-label">Choose</InputLabel>
-          <Select
-            labelId="translation-selector-label"
-            id="translation-selector"
-            value={translation}
-            label="Choose"
-            onChange={handleTranslationChange}
-          >
-            <MenuItem value={"esv"}>ESV</MenuItem>
-            <MenuItem value={"hillbilly"}>'Mater</MenuItem>
-            <MenuItem value={"joker"}>Joker</MenuItem>
-            <MenuItem value={"yoda"}>Yoda</MenuItem>
-            <MenuItem value={"groot"}>Groot</MenuItem>
-          </Select>
-        </FormControl>
-        {/*         <Button
-          variant="contained"
-          size="medium"
-          style={{ marginTop: "20px", height: "2rem" }}
-          onClick={handleGpt}
-        >
-          'Mater
-        </Button> */}
         <Button
-          onClick={() => {
-            setResultVerse("");
-            setGptVerse("");
-            if (translation === "esv") {
-              handleSearch();
-            } else {
-              handleGpt(searchInput, translation);
-            }
           onClick={() => {
             setResultVerse("");
             setGptVerse("");
@@ -143,22 +83,6 @@ function Search() {
           Search
         </Button>
       </Box>
-      <Container fixed>
-        <h1
-          style={{
-            textAlign: "center",
-            fontWeight: "bold",
-            marginBottom: "1rem",
-          }}
-        >
-          Enjoy your translation!
-        </h1>
-        <Container fixed>
-          <p>{resultVerse}</p>
-        </Container>
-
-        <p>{gptVerse}</p>
-      </Container>
       <Container fixed>
         <h1
           style={{
